97564205923c3efe9146b0ce500ee6973d33a6a0
junaietfaruk <junaietfaruk@throughltd.com>
2025-03-17 02:16:46 +0600

    food categori menu

--- src/Pages/OurFood/Food.jsx
+++ src/Pages/OurFood/Food.jsx
@@ -4,36 +4,58 @@ import "react-tabs/style/react-tabs.css";
 import  './Food.css'
 import Sharebanner from "../../Component/Sharebanner/Sharebanner";
 import image from '../../../public/foodb.png'
+import Usemenu from "../../Hooks/Usemenu";
+import FoodCard from "./FoodCard";
 const Food = () => {
     const [tabIndex, setTabIndex] = useState(0);
+    const [popular] = Usemenu()
+    const soupFood = popular.filter(item => item.category === "soup")
+    const saladFood = popular.filter(item => item.category === "salad")
+    const pizzaFood = popular.filter(item => item.category === "pizza")
+    const drinksFood = popular.filter(item => item.category === "drinks")
 
     return (
         <div>
               <Sharebanner img={image} title={"Our Food"} description={"Most Delicious Food in the Restourant"}/>
 
-            <div className="max-w-4xl mx-auto mt-10 p-5">
+            <div className="max-w-screen-xl mx-auto mt-10 p-5">
                 <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
                     {/* Tab List */}
-                    <TabList className="flex gap-4 border-none">
-                        <Tab className={`px-4 py-2 cursor-pointer focus:outline-none ${tabIndex === 0 ? "text-yellow-500 font-bold" : ""}`}>Title 1</Tab>
-                        <Tab className={`px-4 py-2 cursor-pointer focus:outline-none ${tabIndex === 1 ? "text-yellow-500 font-bold" : ""}`}>Title 2</Tab>
-                        <Tab className={`px-4 py-2 cursor-pointer focus:outline-none ${tabIndex === 2 ? "text-yellow-500 font-bold" : ""}`}>Title 3</Tab>
-                        <Tab className={`px-4 py-2 cursor-pointer focus:outline-none ${tabIndex === 3 ? "text-yellow-500 font-bold" : ""}`}>Title 4</Tab>
-                    </TabList>
+                    <TabList className="flex gap-4 border-none justify-center items-center mb-5">
+                        <Tab className={`px-4 py-2 cursor-pointer focus:outline-none ${tabIndex === 0 ? "!text-yellow-500 !font-bold" : ""}`}>Title 1</Tab>
+                        <Tab className={`px-4 py-2 cursor-pointer focus:outline-none ${tabIndex === 1 ? "!text-yellow-500 !font-bold" : ""}`}>Title 2</Tab>
+                        <Tab className={`px-4 py-2 cursor-pointer focus:outline-none ${tabIndex === 2 ? "!text-yellow-500 !font-bold" : ""}`}>Title 3</Tab>
+                        <Tab className={`px-4 py-2 cursor-pointer focus:outline-none ${tabIndex === 3 ? "!text-yellow-500 !font-bold" : ""}`}>Title 4</Tab>
+                        </TabList>
 
                     {/* Tab Panels */}
                     <TabPanel>
-                        <div className="p-4 bg-gray-100 rounded-lg">Content for Tab 1</div>
+                       <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-12">
+                            {
+                                soupFood?.map((item) => <FoodCard key={item._id} item={item} />)
+                            }
+                       </div>
                     </TabPanel>
                     <TabPanel>
-                        <div className="p-4 bg-gray-100 rounded-lg">Content for Tab 2</div>
+                        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-12">
+                            {
+                                 saladFood?.map((item) => <FoodCard key={item._id} item={item} />)
+                            }
+                        </div>
                     </TabPanel>
                     <TabPanel>
-                        <div className="p-4 bg-gray-100 rounded-lg">Content for Tab 3</div>
-                    </TabPanel>
+                        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-12">
+                            {
+                                pizzaFood?.map((item) => <FoodCard key={item._id} item={item} />)
+                            }
+                        </div>               
+                             </TabPanel>
                     <TabPanel>
-                        <div className="p-4 bg-gray-100 rounded-lg">Content for Tab 4</div>
-                    </TabPanel>
+                        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-12">
+                            {
+                                 drinksFood ?.map((item) => <FoodCard key={item._id} item={item} />)
+                            }
+                        </div>                    </TabPanel>
                 </Tabs>
             </div>
         </div>
